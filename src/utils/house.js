363b4cf198fb3542e6a9ba53016a50ab08e520ae
Dean Francis <deanfranci@gmail.com>
2020-09-27 08:05:59 +1000

    House rooms have frames

--- src/utils/house.js
+++ src/utils/house.js
@@ -3,39 +3,45 @@ import {arrayFrom, sample, randomBetween} from './randomizer.js'
 import {blockExists, toCoordinate, isEnclosed} from './blocks.js'
 
 // Random rooms
-const roomCount = 10
+const roomCount = 5
+
+// does this block sit at the start or the end of an array
+const isFrame = (block, start, length) => block === start || block === start + length - 1
 
 const house = (mirrorX) => {
-	const rooms = arrayFrom(randomBetween(2, roomCount)) // [1,2,3..]
+	const rooms = arrayFrom(randomBetween(2, roomCount))
 	const blocks = []
 	const blockLookup = {}
 
-	const main = sample(blockNames)
-	const secondary = sample(blockNames)
-	const tertiary = sample(blockNames)
+	const planks = sample(blockNames.filter((name) => name.match('planks')))
+	const wood = sample(blockNames.filter((name) => name.match('wood')))
 
 	rooms.forEach(() => {
-		const randomMain = sample([main, secondary, tertiary])
-		const randomSecondary = sample([main, secondary, tertiary])
-		const randomTertiary = sample([main, secondary, tertiary])
-		const blockSelection = [randomMain, randomMain, randomMain, randomSecondary, randomTertiary]
+		// const planksSelection = [planks]
 
 		const roomXStart = randomBetween(-3, 3)
-		const roomYStart = 1
+		const roomYStart = randomBetween(1, 3)
 		const roomZStart = randomBetween(-3, 3)
 
 		const roomXLength = randomBetween(3, 5)
-		const roomYLength = randomBetween(3, 5)
-		const roomZLength = randomBetween(3, 5)
+		const roomYLength = randomBetween(3, 7)
+		const roomZLength = randomBetween(3, 7)
 
 		arrayFrom(roomXLength).forEach((ix) => {
 			arrayFrom(roomYLength).forEach((iy) => {
 				arrayFrom(roomZLength).forEach((iz) => {
-					const block = sample(blockSelection)
 					const x = roomXStart + ix
-					const xx = roomXStart + ix - (roomXStart + ix) * 2
+					const xx = x - x * 2
 					const y = roomYStart + iy
 					const z = roomZStart + iz
+
+					const frameX = isFrame(x, roomXStart, roomXLength)
+					const frameY = isFrame(y, roomYStart, roomYLength)
+					const frameZ = isFrame(z, roomZStart, roomZLength)
+					// if at least two blocks sit in a frame position
+					const placeFrame = [frameX, frameY, frameZ].filter((frame) => !!frame).length > 1
+
+					const block = placeFrame ? wood : planks
 
 					if (!blockExists(x, y, z, blockLookup)) {
 						blocks.push({
